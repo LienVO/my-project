0101a4ccae8549ffd299992ebea988b5659b0e59
Lien Valenciano Odio <lvalencianocu@gmail.com>
2018-03-04 15:51:40 +0100

    first commit

---
+++ src/components/home/home.ts
@@ -0,0 +1,16 @@
+import { Component, Vue } from 'vue-property-decorator'
+
+
+import './home.scss'
+
+@Component({
+  template: require('./home.html'),
+  components: {
+
+  }
+})
+export class HomeComponent extends Vue {
+
+
+
+}
---
+++ src/components/navbar/navbar.ts
@@ -0,0 +1,13 @@
+import { Component, Vue, Watch } from 'vue-property-decorator'
+
+import { Link } from './link'
+import { Logger } from '../../util/log'
+
+@Component({
+  template: require('./navbar.html'),
+  components: {
+
+  }
+})
+export class NavbarComponent extends Vue {
+}
