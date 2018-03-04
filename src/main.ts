f4ca5926d200bd57efde099fe9773d9bc23bd572
jalfonso <jalfonso@sorint.it>
2018-03-04 17:40:28 +0100

    add element-ui

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import { makeHot, reload } from './util/hot-reload'
 import { createRouter } from './router'
 
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+
 const navbarComponent = () => import('./components/navbar').then(({ NavbarComponent }) => NavbarComponent)
 // const navbarComponent = () => import(/* webpackChunkName: 'navbar' */'./components/navbar').then(({ NavbarComponent }) => NavbarComponent)
 
 import './sass/main.scss'
+
+Vue.use(ElementUI, { locale })
 
 if (process.env.ENV === 'development' && module.hot) {
   const navbarModuleId = './components/navbar'
